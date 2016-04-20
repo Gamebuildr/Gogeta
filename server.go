4b2c06e73ee0565e1f17b96bc649d1f706ab37cd
Boomer Rogers <boomer@broadsword-publishing.com>
2016-04-20 14:28:04 +0100

    Add initial server file

---
+++ server.go
@@ -0,0 +1,27 @@
+package main;
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+);
+
+func main() {
+    router := mux.NewRouter().StrictSlash(true)
+        router.HandleFunc("/", Index)
+        log.Fatal(http.ListenAndServe(":9000", router));
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path));
+}
+
+// func main() {
+//     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path));
+//     });
+//     log.Fatal(http.ListenAndServe(":9000", nil));
+// }

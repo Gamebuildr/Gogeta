cd0e6af44dea3ba72a99f6a9ab8507a88f2d6453
Boomer <herman.rogers@gmail.com>
2016-04-22 00:29:56 +0100

    Add eureka service url

--- gogeta.go
+++ gogeta.go
@@ -11,8 +11,6 @@ func main() {
 }
 
 func RegisterEureka() {
-    // e, _ := fargo.NewConnFromConfigFile("/etc/fargo.gcfg");
-    // e.AppWatchChannel
-    c := fargo.NewConn("http://127.0.0.1:8080/eureka/v2")
+    c := fargo.NewConn("http://eureka-gamebuildr.herokuapp.com")
     c.GetApps()
 }

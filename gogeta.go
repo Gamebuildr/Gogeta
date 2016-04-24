a021a254f113b3f1ac2e33993d99accd99a87e1b
Boomer <herman.rogers@gmail.com>
2016-04-24 18:59:29 +0100

    Remove fargo and add secondary Eureka client

--- gogeta.go
+++ gogeta.go
@@ -3,8 +3,9 @@ package main;
 import (
     "fmt"
     "os"
+    // "time"
     "github.com/herman-rogers/kingkai"
-    //"github.com/hudl/fargo"
+    "github.com/ArthurHlt/go-eureka-client/eureka"
 );
 
 func main() {
@@ -23,23 +24,34 @@ func GetPort() string {
 }
 
 func RegisterEureka() {
-    //e := fargo.NewConn("http://eureka-gamebuildr.herokuapp.com");
-    // // app, _ := e.GetApp("TESTAPP");
-    //e.GetApps();
-    // fmt.Println(apps);
-    // for k, v := range apps {
-    //     fmt.Println("k:", k, "v:", v);
-    // }
-
-    // e, _ := fargo.NewConnFromConfigFile("/etc/fargo.gcfg")
-    // app, _ := e.GetApp("TESTAPP")
-    // // starts a goroutine that updates the application on poll interval
-    // e.UpdateApp(&app)
-    // for {
-    //     for _, ins := range app.Instances {
-    //         fmt.Printf("%s, ", ins.HostName)
-    //     }
-    //     fmt.Println(len(app.Instances))
-    //     <-time.After(10 * time.Second)
-    // }
+    client := eureka.NewClient([]string {
+        "http://eureka-gamebuildr.herokuapp.com",
+    });
+    instance := eureka.NewInstanceInfo("gogeta.herokuapp.com", "gogeta", "gogeta.herokuapp.com", 80, 30, false);
+    instance.Metadata = &eureka.MetaData {
+        Map: make(map[string]string),
+    }
+    instance.Metadata.Map["foo"] = "bar";
+    client.RegisterInstance("gogeta", instance);
+    client.GetApplication(instance.App);
+    client.GetInstance(instance.App, instance.HostName);
+    client.SendHeartbeat(instance.App, instance.HostName);
 }
+
+// func main() {
+//
+//     client := eureka.NewClient([]string{
+//         "http://127.0.0.1:8761/eureka", //From a spring boot based eureka server
+//         // add others servers here
+//     })
+//     instance := eureka.NewInstanceInfo("test.com", "test", "69.172.200.235", 80, 30, false) //Create a new instance to register
+//     instance.Metadata = &eureka.MetaData{
+//         Map: make(map[string]string),
+//     }
+//     instance.Metadata.Map["foo"] = "bar" //add metadata for example
+//     client.RegisterInstance("myapp", instance) // Register new instance in your eureka(s)
+//     applications, _ := client.GetApplications() // Retrieves all applications from eureka server(s)
+//     client.GetApplication(instance.App) // retrieve the application "test"
+//     client.GetInstance(instance.App, instance.HostName) // retrieve the instance from "test.com" inside "test"" app
+//     client.SendHeartbeat(instance.App, instance.HostName) // say to eureka that your app is alive (here you must send heartbeat before 30 sec)
+// }

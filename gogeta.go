5f5f9a887a6006803ef30900faaeffb40e19675f
Boomer <herman.rogers@gmail.com>
2016-04-24 17:14:32 +0100

    Add test for kingkai server running

--- gogeta.go
+++ gogeta.go
@@ -2,7 +2,7 @@ package main;
 
 import (
     "github.com/herman-rogers/kingkai"
-    "github.com/hudl/fargo"
+    //"github.com/hudl/fargo"
 );
 
 func main() {
@@ -11,6 +11,23 @@ func main() {
 }
 
 func RegisterEureka() {
-    c := fargo.NewConn("http://eureka-gamebuildr.herokuapp.com")
-    c.GetApps()
+    //e := fargo.NewConn("http://eureka-gamebuildr.herokuapp.com");
+    // // app, _ := e.GetApp("TESTAPP");
+    //e.GetApps();
+    // fmt.Println(apps);
+    // for k, v := range apps {
+    //     fmt.Println("k:", k, "v:", v);
+    // }
+
+    // e, _ := fargo.NewConnFromConfigFile("/etc/fargo.gcfg")
+    // app, _ := e.GetApp("TESTAPP")
+    // // starts a goroutine that updates the application on poll interval
+    // e.UpdateApp(&app)
+    // for {
+    //     for _, ins := range app.Instances {
+    //         fmt.Printf("%s, ", ins.HostName)
+    //     }
+    //     fmt.Println(len(app.Instances))
+    //     <-time.After(10 * time.Second)
+    // }
 }

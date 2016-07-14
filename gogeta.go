c54908581ac225e563cac046bd0de8f460a6af04
Boomer <herman.rogers@gmail.com>
2016-07-14 23:02:20 +0100

    Format gogeta.go file

--- gogeta.go
+++ gogeta.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "os"
-    "net/http"
-    "github.com/herman-rogers/gogeta/logger"
-    "github.com/herman-rogers/gogeta/config"
+	"github.com/herman-rogers/gogeta/config"
+	"github.com/herman-rogers/gogeta/logger"
+	"net/http"
+	"os"
 )
 
 func main() {
-    config.Load()
-    StartAppPoller()
-    StartServer()
+	config.Load()
+	StartAppPoller()
+	StartServer()
 }
 
 func StartServer() {
-    var port string = GetPort()
-    logger.Info("Gogeta Server Started")
-    err := http.ListenAndServe(port, nil)
-    logger.LogData(err, "Start Server")
+	var port string = GetPort()
+	logger.Info("Gogeta Server Started")
+	err := http.ListenAndServe(port, nil)
+	logger.LogData(err, "Start Server")
 }
 
 func GetPort() string {
-    var port = os.Getenv("PORT")
-    if (port == "") {
-        port = "9000"
-    }
-    return ":" + port
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+	}
+	return ":" + port
 }

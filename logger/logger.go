ac315ef5b6998f120c30fef2604d44782eccac46
Boomer <herman.rogers@gmail.com>
2017-01-07 15:42:13 +0000

    Fix logger to allow running Gogeta in multiple locations

--- logger/logger.go
+++ logger/logger.go
@@ -3,18 +3,42 @@ package logger
 import (
     "log"
     "os"
+    "path/filepath"
+    "runtime"
     "time"
 )
 
-const fileName string = "gogeta_"
+const DEFAULT_FILE_NAME string = "system_log_"
+
+func GetCustomLogFile(customFile string) *os.File {
+    return CreateLogFile(customFile)
+}
 
 func GetLogFile() *os.File {
+    return CreateLogFile(DEFAULT_FILE_NAME)
+}
+
+func CreateLogFile(file string) *os.File {
     var time = time.Now().Local()
-    var filename string = fileName + time.Format("2006-01-02") + ".log"
-    var directory string = "logs/" + filename
+    var filename string = file + time.Format("2006-01-02"+".log")
+    var _, base, _, _ = runtime.Caller(0)
+    var basePath = filepath.Dir(base)
+    var directory string = basePath + "/logs/" + filename
     logfile, err := os.OpenFile(directory, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0640)
     LogError(err, "Log File Error")
     return logfile
+}
+
+func GetLogDirectory() string {
+    var _, base, _, _ = runtime.Caller(0)
+    var basePath = filepath.Dir(base)
+    return basePath + "/logs/"
+}
+
+func TimestampFilename(file string) string {
+    var time = time.Now().Local()
+    var filename string = file + time.Format("2006-01-02"+".log")
+    return filename
 }
 
 func LogData(err error, info string) {

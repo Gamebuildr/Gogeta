b3f8a955a22c4033f21f1d68bf787de0383af26a
Boomer Rogers <herman.rogers@gmail.com>
2017-01-31 18:37:14 +0000

    Have source system return errors directly and remove source use cases

--- examples/examples.go
+++ examples/examples.go
@@ -26,7 +26,15 @@ func SourceControlExample() {
 
 // LoggerExample shows how to implement the logger interface
 func LoggerExample() {
+	// Create a new log save system that will persist our log data
 	fileLogger := logger.FileLogSave{LogFileName: "system_log_"}
-	devLog := new(logger.DevLog)
-	devLog.Log = logger.NewSystemLogger(fileLogger)
+
+	// Create a new logging system to format our data
+	logger := new(logger.SystemLogger)
+
+	// Setup the logsave function to our file logger
+	logger.LogSave = fileLogger
+
+	// Use of the logger will store data into a file
+	logger.Info("Logger System is Saving to File")
 }
--- pkg/sourcesystem/sourcesystem.go
+++
@@ -1,7 +0,0 @@
-package sourcesystem
-
-// DevSourceSystem is a dev test system to implement
-// new architecture
-type DevSourceSystem struct {
-	SourceSystem SourceSystem
-}

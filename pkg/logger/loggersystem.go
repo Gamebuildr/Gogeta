f85e03bac1ce5345fdb82648796dc57e9fcb2f29
Boomer Rogers <herman.rogers@gmail.com>
2017-02-02 14:03:26 +0000

    Remove logger from system

--- pkg/logger/loggersystem.go
+++
@@ -1,8 +0,0 @@
-package logger
-
-// Log is the abstraction for sending
-// and collecting log data from the system
-type Log interface {
-	Info(message string) string
-	Error(message string) string
-}

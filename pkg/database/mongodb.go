9d72e2bb78aec6c219a62e367b4ab7f986d9816d
Boomer <herman.rogers@gmail.com>
2017-03-11 18:55:11 +0000

    Add dockerfile setup

--- pkg/database/mongodb.go
+++ pkg/database/mongodb.go
@@ -1,11 +1,6 @@
 package database
 
-import mgo "gopkg.in/mgo.v2"
-
-func ConnectToMongoDB() *mgo.Session {
-	session, err := mgo.Dial("mongodb://localhost:27017/gogeta")
-	if err != nil {
-		// logger.Error("MongoDB Error: " + err.Error())
-	}
-	return session
+func ConnectToMongoDB() {
+	// session, err := mgo.Dial("mongodb://localhost:27017/gogeta")
+	// return session
 }
